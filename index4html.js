77009365d066a53dd031149e3c22037a49dd1ff9
Carmen Huitrón <135810760+carmen09h@users.noreply.github.com>
2024-05-28 15:17:31 -0600

    Coding implementing Bootstrap.

---
+++ index4html.js
@@ -0,0 +1,27 @@
+//ESCRITURA DE LAS FUNCIONES
+//FUNCION 1. Generacion de un numero aleatorio 
+function generaColor(){
+//Genera el valor del RGB por separado y guardarlos en varianbles
+
+const rojo = Mith.floor();
+// lineas de codigo 1 y 2
+// 1: const verde =Mith floor();
+// 2: const azul =;
+/// Valor RGB como resultado de x,y,z
+}
+
+///*********************************************************************/
+// FUNCION 2. Cambiar el color del cuerpo de la pagina 
+// ACCEDIENDO AL COLOR
+function cambioBackgroud(){
+    // generaColor();
+}
+
+///*************************************************************************** */
+// Buscar el boton en mi html por su clase 
+// Cada vez que mi boton sea accionado, voy a llamar o ejecutar 
+//mi funcion cambioBackgroud();
+
+
+
+
